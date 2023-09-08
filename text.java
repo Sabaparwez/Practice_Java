2256a83ede1a7c3e759738f2d38d650d636cee9b
Sabaparwez <67377394+Sabaparwez@users.noreply.github.com>
2023-09-08 22:01:32 +0530

    myfirstprogram

---
+++ text.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test
+{
+ public static void main (String[] args)
+	{
+	 int length = 11;
+        int width = 13;
+        System.out.println(length * width);
+        System.out.println(2 * (length + width));
+        width = 15; 
+        System.out.println(length * width);
+        System.out.println(2 * (length + width));
+	}
+}
